ce50ce76a4e4817aaa0981d6e47e05c5fc51f9cc
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-01-25 22:34:38 +0900

    [level 2] Title: 더 맵게, Time: 1410.34 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for (int i = 0; i < scoville.length; i++) {
+            q.add(scoville[i]);
+        }
+        while(q.peek() < K) {
+            if (q.size() == 1)
+                return -1;
+            q.add(q.poll() + q.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+}
