afe2a3e82ed1df752008f05f98e9c36b6c6b46e2
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-04-27 19:15:03 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,36 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int[] students = new int[n];
+        int lostCount = lost.length;
+        Arrays.fill(students, 1);
+        for(int lostElement: lost) {
+            students[lostElement - 1]--;
+        }
+        for(int reserveElement: reserve) {
+            students[reserveElement - 1]++;
+        }
+        for(int i = 0; i < n; i++) {
+            if(students[i] == 0) {
+                if(i > 0 && students[i - 1] == 2) {
+                    students[i]++;
+                    students[i - 1]--;
+                    lostCount--;
+                } else if(i < n - 1 && students[i + 1] == 2) {
+                    students[i]++;
+                    students[i + 1]--;
+                    lostCount--;
+                }
+            }
+        }
         int answer = 0;
-        
-        Arrays.sort(reserve);
-        Arrays.sort(lost);
-        
-        answer = n - lost.length;
-        
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] == reserve[j]) {
-					answer++;
-					lost[i] = -1;
-					reserve[j] = -1;
-                    break;
-				}
-			}
-		}
-        
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-					answer++;
-					reserve[j] = -1;
-					break;
-				}
-			}
-		}
-        
+        for(int i = 0; i < n; i++) {
+            if(students[i] >= 1) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
