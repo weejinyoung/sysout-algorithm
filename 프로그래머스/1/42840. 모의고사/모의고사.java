5d28aa60678f3079f467e566d90df81ec12d81ad
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-04-27 19:52:24 +0900

    [level 1] Title: 모의고사, Time: 3.18 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,27 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5}; 
+        int[] score = new int[3];
+        Arrays.fill(score, 0);
+        int[] first = {1,2,3,4,5};
         int[] second = {2,1,2,3,2,4,2,5};
-        int[] third = {3,3,1,1,2,2,4,4,5,5}; 
-        int[] score = {0,0,0}; 
-        
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == first[i%5]) score[0]++;
-            if(answers[i] == second[i%8]) score[1]++;
-            if(answers[i] == third[i%10]) score[2]++;
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        for(int i = 0; i < answers.length; i++) {
+            if(first[i % 5] == answers[i]) {
+                score[0]++;
+            }
+            if(second[i % 8] == answers[i]) {
+                score[1]++;
+            }
+            if(third[i % 10] == answers[i]) {
+                score[2]++;
+            }
         }
         
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        
-        List<Integer> answ = new ArrayList<Integer>();
-        for(int i=0; i<score.length; i++) if(max == score[i]) answ.add(i+1);
-        
-        int[] answer = new int[answ.size()];
-        for(int i=0; i<answ.size(); i++){
-            answer[i] = answ.get(i);
+        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>(Collections.reverseOrder());
+        for(int i = 0; i < score.length; i++) {
+            if(treeMap.containsKey(score[i])) {
+                treeMap.get(score[i]).add(i + 1);
+            } else {
+                List<Integer> newList = new ArrayList<>();
+                newList.add(i + 1);
+                treeMap.put(score[i], newList);
+            }
         }
-
-        return answer;
+        return treeMap.firstEntry().getValue().stream().mapToInt(i -> i).toArray();
     }
 }
