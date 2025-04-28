9625fa1b0b293a34739b12ed45994150f667c385
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-04-28 21:48:37 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2200.79 ms, Memory: 678 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,33 +1,60 @@
-public class Solution {
+import java.util.*;
+
+class Solution {
     public String solution(String X, String Y) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        int[] arrX = new int[10];
-        int[] arrY = new int[10];
-
-        for(String a : X.split("")){
-            arrX[Integer.parseInt(a)]++; 
+        String[] xArray = X.split("");
+        String[] yArray = Y.split("");
+        Map<String, Integer> xMap = new HashMap<>();
+        Map<String, Integer> yMap = new HashMap<>();
+        for(int i = 0; i < xArray.length; i++) {
+            if(xMap.containsKey(xArray[i])) {
+                int value = xMap.get(xArray[i]);
+                value++;
+                xMap.put(xArray[i], value);
+            } else {
+                xMap.put(xArray[i], 1);
+            }
         }
-
-        for(String b : Y.split("")){
-            arrY[Integer.parseInt(b)]++; 
+        for(int i = 0; i < yArray.length; i++) {
+            if(yMap.containsKey(yArray[i])) {
+                int value = yMap.get(yArray[i]);
+                value++;
+                yMap.put(yArray[i], value);
+            } else {
+                yMap.put(yArray[i], 1);
+            }
         }
-
-        for(int i=9; i>=0; i--){
-            if(arrX[i]>0 && arrY[i]>0){
-                int tmpInt = Math.min(arrX[i], arrY[i]);
-                for(int j=0; j<tmpInt; j++){
-                    sb.append(i);
+        int xLength = xArray.length;
+        int yLength = yArray.length;
+        PriorityQueue<String> pq = new PriorityQueue<>(Collections.reverseOrder());
+        if(xLength <= yLength) {
+            for(String key : xMap.keySet()) {
+                if(yMap.containsKey(key)) {
+                    for(int i = 0; i < Math.min(yMap.get(key), xMap.get(key)); i++) {
+                        pq.offer(key);
+                    }
+                }
+            }
+        } else {
+            for(String key : yMap.keySet()) {
+                if(xMap.containsKey(key)) {
+                    for(int i = 0; i < Math.min(yMap.get(key), xMap.get(key)); i++) {
+                        pq.offer(key);
+                    }
                 }
             }
         }
-
-        if (sb.length()>0){
-            answer = sb.charAt(0)=='0'? "0": sb.toString();
-        } else {
-            answer = "-1";
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()) {
+            sb.append(pq.poll());
         }
-
+        String answer = sb.toString();
+        if(answer.equals("")) {
+            answer = "-1";
+        } 
+        if(answer.charAt(0) == '0') {
+            answer = "0";
+        }
         return answer;
     }
 }
