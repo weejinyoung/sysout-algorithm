931a0f98bd19d27d26a69f0c0523cdacd22bda80
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-05-07 00:44:18 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,32 @@
+import java.util.*;
 class Solution {
+    
+    boolean[] visited;
+    int networkCount = 0;
+    
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(computers, visited, i);
-                count++;
+        visited = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                bfs(i, computers);
+                networkCount++;
             }
         }
-        
-        return count;
+        return networkCount;
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int current) {
-        visited[current] = true;
+    public void bfs(int start, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[start] = true;
+        q.add(start);
         
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[current][i] == 1 && !visited[i]) {
-                dfs(computers, visited, i);
+        while(!q.isEmpty()) {
+            int current = q.poll();
+            for(int i = 0; i < computers[current].length; i++) {
+                if(computers[current][i] == 1 && !visited[i] && current != i) {
+                    visited[i] = true;
+                    q.add(i);
+                }
             }
         }
     }
