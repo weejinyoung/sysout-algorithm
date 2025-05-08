2da504c8f12f8b2b96e3997067219a9e24800de9
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-05-08 16:40:25 +0900

    [level 2] Title: 피로도, Time: 3.22 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.io.*;
 
 class Solution {
-
-    static int answer = 0;
-    static boolean[] visited;
     
-    public int solution(int k, int[][] dungeons) {      
+    int result = 0;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
         dfs(0, k, dungeons);
-        return answer;
+        return result;
     }
     
-    public void dfs(int cnt, int fatigue, int[][] dg) {
-        for(int i = 0; i < dg.length; i++){
-            if(visited[i] || dg[i][0] > fatigue) continue;
+    public void dfs(int count, int k, int[][] dungeons) {
+        result = Math.max(result, count);
+        if(k <= 0 || count >= dungeons.length) {
+            return;
+        }
+        for(int i = 0; i < dungeons.length; i++) {
+            if(visited[i] || k < dungeons[i][0]) continue;
             visited[i] = true;
-            dfs(cnt+1, fatigue-dg[i][1], dg);
+            dfs(count + 1, k - dungeons[i][1], dungeons);
             visited[i] = false;
         }
-        answer = Math.max(cnt, answer);
     }
 }
