861edde5896b69793f416a0faec8be354a261334
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-05-08 17:52:36 +0900

    [level 2] Title: 모음 사전, Time: 43.11 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,32 +1,34 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
-    static List<String> list;
-    static String [] words = {"A", "E", "I", "O", "U"};
+    String[] alphabats = {"A", "E", "I", "O", "U"};
+    TreeSet<String> dictionary = new TreeSet<>();
+    int result = 0;
     
     public int solution(String word) {
-        int answer = 0;
-        list = new ArrayList<>();
-        dfs("", 0);
-        int size = list.size();
-        
-        for (int i = 0; i < size; i++) {
-            if (list.get(i).equals(word)) {
-                answer = i;
-                break;
+        dfs(0, "");
+        int count = 1;
+        for(String elmnt : dictionary) {
+            if(word.equals(elmnt)) {
+                return count;
             }
+            count++;
         }
-        
-        return answer;
+        return 0;
     }
-
-    static void dfs(String str, int len) {
-        list.add(str);
-        if (len == 5) return;
-        
-        for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
+    
+    public void dfs(int depth, String current) {
+        if(depth > 4) {
+            if(!current.equals("")) {
+                dictionary.add(current);
+            }
+            return;
+        }
+        for(int i = 0; i < alphabats.length; i++) {
+            dfs(depth + 1, current + alphabats[i]);
+            dfs(depth + 1, current);
         }
     }
 }
